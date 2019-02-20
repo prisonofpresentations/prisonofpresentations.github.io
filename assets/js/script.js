dc96478155e7565f74d57d73c475137a91dc15fa
Anmol Modur <asm7148@rit.edu>
2019-02-20 15:39:08 -0500

    Making Transitions through JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
-AOS.init({
-    duration: 1200,
-})
+$(".page").children().each(function (i, ele) {
+    // console.log(ele);
+    $(ele).attr("data-aos", "fade-up");
+});
+
+
+$(document).ready(function () {
+    AOS.init({
+        duration: 1200,
+    })
+});
